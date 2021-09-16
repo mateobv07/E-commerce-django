58f452425de7db5e4c1dabd85992c1c981faa94a
unknown <bernasconivargas.mateo@gmail.com>
2021-09-16 15:53:56 -0500

    variation stuff

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from carts.models import CartItem
 from category.models import Category
 from django.shortcuts import get_object_or_404, render
@@ -45,4 +46,17 @@ def product_detail(request, category_slug, product_slug):
         'in_cart'       : in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    
+    context = {
+        'products': products,
+        'product_count':product_count
+    }
+    return render(request, 'store/store.html', context)
 
