5e7e631088024e2ea63c3a4c110b84d918d76872
unknown <bernasconivargas.mateo@gmail.com>
2021-10-29 10:49:07 -0500

    Only 8 products homepage

--- ecom_django/views.py
+++ ecom_django/views.py
@@ -3,7 +3,7 @@ from store.models import Product
 from store.models import ReviewRating, Product
 
 def home(request):
-    products = Product.objects.all().filter(is_available = True).order_by('created_date')
+    products = Product.objects.all().filter(is_available = True).order_by('-created_date')[:10]
 
 
     context = {
